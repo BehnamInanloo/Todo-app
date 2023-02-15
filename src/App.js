761f99b40f7350aea42f5b74d732203ef0a200f0
Behnam Inanloo <behnaminanloo@yahoo.com>
2023-02-15 10:31:16 +0330

    modify mediaQuery

--- src/App.js
+++ src/App.js
@@ -9,22 +9,20 @@ const App = () => {
   ])
 
   return (
-    <div className='bg-todo-main'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-6 my-5 my-md-0'>
-            <div className='h-100 w-100 d-flex justify-content-center align-items-center'>
-              <TypeTodo setTodoList={setTodoList} todoList={todoList} />
-            </div>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6 my-5 my-md-0'>
+          <div className='h-100 w-100 d-flex justify-content-center align-items-center'>
+            <TypeTodo setTodoList={setTodoList} todoList={todoList} />
           </div>
-          <div className='col-md-6 d-flex align-items-center 
+        </div>
+        <div className='col-md-6 d-flex align-items-center 
             justify-content-center mb-5 mb-md-0 todo-main-box'>
-            <div className='w-100 todo-boxes'>
-              {todoList.map((item) => (
-                <Todo title={item.title} text={item.text} key={item.id}
-                  setTodoList={setTodoList} todoList={todoList} id={item.id} />
-              ))}
-            </div>
+          <div className='w-100 todo-boxes'>
+            {todoList.map((item) => (
+              <Todo title={item.title} text={item.text} key={item.id}
+                setTodoList={setTodoList} todoList={todoList} id={item.id} />
+            ))}
           </div>
         </div>
       </div>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -11,7 +11,7 @@ const Todo = ({ title, text, id, todoList, setTodoList }) => {
     <div className='mb-4 mx-auto shadow-sm todo-box'>
       <div className='d-flex justify-content-between align-items-center px-3 py-1 w-100 rounded-top todo-title'>
         <h3 className='text-white mb-0'>{title}</h3>
-        <button className='btn pe-0' title='Delete todo'
+        <button className='btn px-0 border-0' title='Delete todo'
           onClick={handleDeleteTodo}
         >
           <FaTrash color='red' />
